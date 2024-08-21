449347a20d653d8dbb8e700efa8d4b181bdc0da9
Drozd0f <600dan600@gmail.com>
2024-08-21 12:18:03 +0300

    Add emoji

--- muzlag/pkg/emoji/emoji.go
+++ muzlag/pkg/emoji/emoji.go
@@ -1,3 +1,52 @@
 package emoji
 
 type Emoji string
+
+const (
+	AngerDefaultEmoji          Emoji = ":anger:"
+	CoffeeDefaultEmoji         Emoji = ":coffee:"
+	JapaneseGoblinDefaultEmoji Emoji = ":japanese_goblin:"
+	KeycapDefaultEmoji         Emoji = "\u20E3"
+	KissingHeartDefaultEmoji   Emoji = ":kissing_heart:"
+	MusicalNoteDefaultEmoji    Emoji = ":musical_note:"
+	OKDefaultEmoji             Emoji = ":ok:"
+	ScreamCatDefaultEmoji      Emoji = ":scream_cat:"
+	SleepingDefaultEmoji       Emoji = ":sleeping:"
+	ThinkingDefaultEmoji       Emoji = ":thinking:"
+	WhiteCheckMarkDefaultEmoji Emoji = ":white_check_mark:"
+)
+
+const (
+	VibingEmoji         Emoji = "<a:vibing:747680206734622740>"
+	CrabraveEmoji       Emoji = "<a:crabrave:739615014679347201>"
+	FeelsBeatsManEmoji  Emoji = "<:FeelsBeatsMan:597591202614738947>"
+	AbufferingEmoji     Emoji = "<a:abuffering:744345725013590098>"
+	SquirtleHypeEmoji   Emoji = "<a:squirtleHype:739616791084400701>"
+	LoadingEmoji        Emoji = "<a:typing:597589448607399949>"
+	SadCatThumbSupEmoji Emoji = "<:sadcatthumbsup:759934286097809428>"
+	YesEmoji            Emoji = "<:yes:597590985802907658>"
+)
+
+const (
+	RemDanceCenterEmoji   Emoji = "<a:rem_dance:1025461780543189042>"
+	AnimeeyesCenterEmoji  Emoji = "<a:animeeyes:1025463582051278909>"
+	NezukaCenterEmoji     Emoji = "<a:nezuka:1025463620492075058>"
+	NqnmCenterEmoji       Emoji = "<a:nqnm:1025463852458053642>"
+	ChikaCenterEmoji      Emoji = "<a:anime:1025463750926553149>"
+	AnimeDanceCenterEmoji Emoji = "<a:anime_dance:1025463771113725962>"
+	AnimerainCenterEmoji  Emoji = "<a:animerain:1025463709046407209>"
+	GhubujiiCenterEmoji   Emoji = "<a:ghubujii:1025463833730486413>"
+	JavidanceCenterEmoji  Emoji = "<a:javidance:1025463813266477216>"
+	AnimeahhhCenterEmoji  Emoji = "<a:animeahhh:1025463792609542225>"
+)
+
+const (
+	ArrowRightPaginationEmoji Emoji = "\U000025b6"
+	ArrowLeftPaginationEmoji  Emoji = "\U000025c0"
+)
+
+const (
+	HelpHelpEmoji Emoji = "<:thrinking:597590667669274651>"
+	HomeHelpEmoji Emoji = "<a:rooSnap:741590910361403454>"
+	PingHelpEmoji Emoji = "<:blobpats:596576796594667521>"
+)
